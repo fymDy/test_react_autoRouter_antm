6c3abeb203f7d3854062c0ca4bd5c0a95ba34424
Fym <Fym@gmail.com>
2024-06-01 08:53:43 +0800

    1 分环境打包 2 设置路径别名

--- craco.config.js
+++ craco.config.js
@@ -64,6 +64,45 @@ module.exports = {
           },
         });
       }
+       // 配置别名
+       webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      };
+
+        // 根据环境变量设置不同的输出目录和文件名格式
+        const isProduction = env === 'production';
+      // 根据环境变量设置不同的输出目录和文件名格式
+      if (isProduction) {
+        //todo分模块打包 
+        webpackConfig.optimization = {
+          splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 70000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            automaticNameDelimiter: '~',
+            cacheGroups: {
+              defaultVendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+                reuseExistingChunk: true,
+                name(module) {
+                  const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                  return `npm.${packageName.replace('@', '')}`;
+                },
+              },
+              default: {
+                minChunks: 2,
+                priority: -20,
+                reuseExistingChunk: true,
+              },
+            },
+          },
+        };
+      }
   // 修改输出文件名和输出目录
   webpackConfig.output = {
     ...webpackConfig.output,
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
 
   return (
     <div className="container">
+       <h1>API URL: {process.env.REACT_APP_API_URL}</h1>
       <AppRoutes />
     </div>
   );
